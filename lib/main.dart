30fb71d5f8dc2da767e090f26057c5fcaf4010c3
jay <yongje812@gmail.com>
2025-03-05 10:29:44 +0900

    [add]SplashView 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:pomo_daily/splash_view.dart';
+import 'package:pomo_daily/theme/app_colors.dart';
+import 'package:pomo_daily/ui/home/home_view.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ProviderScope(child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -11,56 +15,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: AppColors.primaryColor),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      home: SplashView(),
+      onGenerateRoute: (settings) {
+        switch (settings.name) {
+          case '/':
+            return MaterialPageRoute(builder: (_) => const SplashView());
+          case '/home':
+            return MaterialPageRoute(builder: (_) => const HomeView());
+        }
+        return null;
+      },
     );
   }
 }
---
+++ lib/splash_view.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:pomo_daily/theme/app_colors.dart';
+
+/// Splash 상태를 관리하는 Provider
+final splashProvider = FutureProvider<bool>((ref) async {
+  await Future.delayed(Duration(seconds: 3)); // 3초 대기 (API 호출 가능)
+  return true; // 로딩 완료
+});
+
+class SplashView extends ConsumerWidget {
+  const SplashView({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final splashState = ref.watch(splashProvider);
+
+    splashState.when(
+      data: (isLoaded) {
+        if (isLoaded) {
+          WidgetsBinding.instance.addPostFrameCallback((_) {
+            Navigator.pushReplacementNamed(context, '/home'); // 홈으로 이동
+          });
+        }
+        return SizedBox.shrink();
+      },
+      loading: () => _buildSplashScreen(), // 로딩 중 화면
+      error: (err, stack) => Center(child: Text('Error: $err')),
+    );
+
+    return Scaffold(body: _buildSplashScreen());
+  }
+
+  /// Splash 화면 UI
+  Widget _buildSplashScreen() {
+    return Container(
+      color: AppColors.primaryColor,
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Pomo Daily',
+              style: TextStyle(fontSize: 30, color: Colors.white),
+            ),
+            SizedBox(height: 20),
+            CircularProgressIndicator(color: Colors.white),
+          ],
+        ),
+      ),
+    );
+  }
+}
