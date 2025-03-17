d6ee6c21924e517d14a2654245f276566c80dcb0
YONGJE JOE <jay@YONGJEui-MacBookPro.local>
2025-03-17 15:28:23 +0900

    [add]CustomSlider 추가

---
+++ lib/ui/widgets/common/custom_slider.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:pomo_daily/config/theme/app_colors.dart';
+
+class CustomSlider extends StatefulWidget {
+  const CustomSlider({
+    super.key,
+    this.value = 5,
+    this.labelSuffix,
+    this.min = 5,
+    this.max = 60,
+    this.division = 11,
+    required this.onChanged,
+  });
+  final double value;
+  final String? labelSuffix;
+  final double min;
+  final double max;
+  final int division;
+
+  final void Function(double) onChanged;
+
+  @override
+  CustomSliderState createState() => CustomSliderState();
+}
+
+class CustomSliderState extends State<CustomSlider> {
+  late double currentValue; // Declare currentValue
+
+  @override
+  void initState() {
+    super.initState();
+    currentValue = widget.value; // Initialize currentValue
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SliderTheme(
+      data: SliderThemeData(
+        trackHeight: 12,
+        activeTrackColor: AppColors.primaryColor,
+        inactiveTrackColor: AppColors.borderGray,
+        thumbColor: AppColors.whiteColor,
+        valueIndicatorColor: AppColors.primaryColor,
+        valueIndicatorTextStyle: TextStyle(color: AppColors.whiteColor),
+        tickMarkShape: RoundSliderTickMarkShape(tickMarkRadius: 0),
+      ),
+      child: Slider(
+        value: currentValue,
+        min: widget.min,
+        max: widget.max,
+        divisions: widget.division,
+        label: '${currentValue.toInt()}${widget.labelSuffix ?? ''}',
+        onChanged: (value) {
+          setState(() {
+            currentValue = value;
+          });
+        },
+      ),
+    );
+  }
+}
--- lib/ui/widgets/common/svg_icon.dart
+++ lib/ui/widgets/common/svg_icon.dart
@@ -5,20 +5,25 @@ import 'package:pomo_daily/config/theme/app_colors.dart';
 class SvgIcon extends StatelessWidget {
   const SvgIcon({
     super.key,
+    this.onTap,
     required this.iconName,
     this.size = 10.0,
     this.iconColor = AppColors.textPrimary,
   });
+  final GestureTapCallback? onTap;
   final String iconName;
   final double? size;
   final Color iconColor;
   @override
   Widget build(BuildContext context) {
-    return SvgPicture.asset(
-      'assets/icons/$iconName.svg',
-      width: size,
-      height: size,
-      colorFilter: ColorFilter.mode(iconColor, BlendMode.srcIn),
+    return GestureDetector(
+      onTap: onTap,
+      child: SvgPicture.asset(
+        'assets/icons/$iconName.svg',
+        width: size,
+        height: size,
+        colorFilter: ColorFilter.mode(iconColor, BlendMode.srcIn),
+      ),
     );
   }
 }
